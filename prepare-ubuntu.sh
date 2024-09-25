09ba7bdb0309688364b678f1b9a28a53487d6b08
ComplYue <compl.yue@icloud.com>
2024-09-25 09:42:48 +0000

    linux sys dep upd

--- prepare-ubuntu.sh
+++ prepare-ubuntu.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get install -y git build-essential cmake ninja-build jq wget curl
-apt-get install -y zlib1g-dev libtinfo-dev libz-dev libzstd-dev libssl-dev
+apt-get install -y libedit-dev zlib1g-dev libz-dev libzstd-dev libssl-dev
