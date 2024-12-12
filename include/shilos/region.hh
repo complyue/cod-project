9c8cf122ba5cdfb3a18de0fa17a36c9c70140d51
ComplYue <compl.yue@icloud.com>
2024-12-12 20:49:16 +0800

    region to have root type as template param

--- include/shilos/region.hh
+++ include/shilos/region.hh
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <cassert>
+#include <concepts>
 #include <cstddef>
 #include <cstdint>
 #include <memory>
@@ -9,12 +10,14 @@
 #include <stdexcept>
 #include <utility>
 
+#include "./uuid.hh"
+
 namespace shilos {
 
 using std::intptr_t;
 using std::size_t;
 
-template <typename T> class global_ptr;
+template <typename VT, typename RT> class global_ptr;
 
 //
 // region-internal pointer fields should be declared as this type,
@@ -26,11 +29,11 @@ template <typename T> class global_ptr;
 //
 // actually, you can do virtually nothing with a regional_ptr<F> alone, and this is by design
 //
-template <typename T> class regional_ptr final {
-  template <typename O> friend class global_ptr;
+template <typename VT> class regional_ptr final {
+  template <typename OT, typename RT> friend class global_ptr;
 
 public:
-  typedef T target_type;
+  typedef VT target_type;
 
 private:
   size_t offset_;
@@ -48,39 +51,49 @@ public:
   // prohibit direct copying and assignment,
   // regional_ptr fields can only be updated via a global_ptr to its parent record:
   //
-  //   const global_ptr<F>& global_ptr<T>::set(
-  //     regional_ptr<F> T::*ptrField, const global_ptr<F> &tgt
-  //   );
-  //
-  regional_ptr(const regional_ptr<T> &) = delete;
-  regional_ptr(regional_ptr<T> &&) = delete;
-  regional_ptr &operator=(const regional_ptr<T> &) = delete;
-  regional_ptr &operator=(regional_ptr<T> &&) = delete;
+  regional_ptr(const regional_ptr<VT> &) = delete;
+  regional_ptr(regional_ptr<VT> &&) = delete;
+  regional_ptr &operator=(const regional_ptr<VT> &) = delete;
+  regional_ptr &operator=(regional_ptr<VT> &&) = delete;
 
   explicit operator bool() const noexcept { return offset_ != 0; }
 };
 
+template <typename RT>
+concept ValidRegionRootType = requires {
+  { RT::TYPE_UUID } -> std::same_as<UUID>;
+};
+
+template <typename RT>
+  requires ValidRegionRootType<RT>
 class memory_region {
-  template <typename T> friend class global_ptr;
+  template <typename VT, RT> friend class global_ptr;
 
 public:
-  static memory_region *alloc_region(const size_t payload_capacity,
-                                     std::allocator<std::byte> allocator = std::allocator<std::byte>()) {
+  template <typename... Args>
+  static memory_region<RT> *alloc_region(const size_t payload_capacity, Args &&...args,
+                                         std::allocator<std::byte> allocator = std::allocator<std::byte>()) {
     const size_t capacity = sizeof(memory_region) + payload_capacity;
     void *ptr = allocator.allocate(capacity);
-    new (ptr) memory_region(capacity);
-    return reinterpret_cast<memory_region *>(ptr);
+    new (ptr) memory_region(capacity, std::forward<Args>(args)...);
+    return reinterpret_cast<memory_region<RT> *>(ptr);
   }
 
 protected:
-  size_t magic_;
+  UUID rt_uuid_;
   size_t capacity_;
   size_t occupation_;
-  size_t root_offset_;
+  size_t ro_offset_;
 
-  memory_region(size_t capacity, size_t magic = 0xCAFEFEEDFACE0101, size_t occupation = sizeof(memory_region),
-                size_t root_offset = 0)
-      : magic_(magic), capacity_(capacity), occupation_(occupation), root_offset_(root_offset) {}
+  template <typename... Args>
+  memory_region(size_t capacity, Args &&...args)
+      : rt_uuid_(RT::TYPE_UUID), capacity_(capacity), occupation_(sizeof(memory_region<RT>)) {
+    void *ptr = allocate(sizeof(RT), alignof(RT));
+    if (!ptr)
+      throw std::bad_alloc();
+    new (ptr) RT(std::forward<Args>(args)...);
+    ro_offset_ = reinterpret_cast<intptr_t>(ptr) - reinterpret_cast<intptr_t>(this);
+  }
 
 public:
   ~memory_region() = default;
@@ -106,56 +119,51 @@ public:
     return ptr;
   }
 
-  template <typename T, typename... Args> global_ptr<T> &&create(Args &&...args) {
-    void *ptr = this->allocate(sizeof(T), alignof(T));
+  template <typename VT, typename... Args> global_ptr<VT, RT> create(Args &&...args) {
+    void *ptr = this->allocate(sizeof(VT), alignof(VT));
     if (!ptr)
       throw std::bad_alloc();
-    new (ptr) T(std::forward<Args>(args)...);
-    return std::move(global_ptr<T>(this, reinterpret_cast<intptr_t>(ptr) - reinterpret_cast<intptr_t>(this)));
+    new (ptr) VT(std::forward<Args>(args)...);
+    return global_ptr<VT, RT>( //
+        this,                  //
+        reinterpret_cast<intptr_t>(ptr) - reinterpret_cast<intptr_t>(this));
+  }
+
+  global_ptr<RT, RT> root() { //
+    return std::move(global_ptr<RT, RT>(this, ro_offset_));
+  }
+
+  template <typename VT> //
+  global_ptr<VT, RT> null() {
+    return std::move(global_ptr<VT, RT>(this, 0));
   }
 };
 
-template <typename T> class global_ptr final {
+template <typename VT, typename RT> class global_ptr final {
 public:
-  typedef T target_type;
-
-  static global_ptr<T> &&null_of(memory_region *region) { //
-    return std::move(global_ptr<T>(region, 0));
-  }
-  static global_ptr<T> &&root_of(memory_region *region) {
-    // TODO: how to verify root type is really T, but without dynamic_cast<>()?
-    //       rtti is not trivially relocatable across processes, as to be mmap-ped at different addresses,
-    //       we don't store rtti in data files accessed via mmap
-    return std::move(global_ptr<T>(region, region->root_offset_));
-  }
+  typedef VT target_type;
 
 private:
-  memory_region *region_;
+  memory_region<RT> *region_;
   size_t offset_;
 
-  global_ptr(memory_region *region, size_t offset) : region_(region), offset_(offset) {}
+  global_ptr(memory_region<RT> *region, size_t offset) : region_(region), offset_(offset) {}
 
 public:
   ~global_ptr() = default;
   // global_ptr can be freely used as both lvalue and rvalue
-  global_ptr(const global_ptr<T> &) = default;
-  global_ptr(global_ptr<T> &&) = default;
-  global_ptr &operator=(const global_ptr<T> &) = default;
-  global_ptr &operator=(global_ptr<T> &&) = default;
-
-  void set_as_root() {
-    // TODO: verify the type is suitable as root object of the region?
-    //       possibly by magic of the region?
-    region_->root_offset_ = offset_;
-  }
+  global_ptr(const global_ptr<VT, RT> &) = default;
+  global_ptr(global_ptr<VT, RT> &&) = default;
+  global_ptr &operator=(const global_ptr<VT, RT> &) = default;
+  global_ptr &operator=(global_ptr<VT, RT> &&) = default;
 
   template <typename F> //
-  void clear(regional_ptr<F> T::*ptrField) {
+  void clear(regional_ptr<F> VT::*ptrField) {
     this->*ptrField.offset_ = 0;
   }
 
   template <typename F> //
-  const global_ptr<F> &set(regional_ptr<F> T::*ptrField, const global_ptr<F> &tgt) {
+  const global_ptr<F, RT> &set(regional_ptr<F> VT::*ptrField, const global_ptr<F, RT> &tgt) {
     if (tgt.region_ != region_) {
       throw std::logic_error("!?cross region ptr assignment?!");
     }
@@ -164,37 +172,41 @@ public:
   }
 
   template <typename F> //
-  global_ptr<F> &&get(regional_ptr<F> T::*ptrField) {
-    return std::move(global_ptr<F>(region_, this->*ptrField.offset_));
+  global_ptr<F, RT> get(regional_ptr<F> VT::*ptrField) {
+    return global_ptr<F, RT>(region_, this->*ptrField.offset_);
   }
 
   template <typename F> //
-  const global_ptr<F> get(regional_ptr<F> T::*ptrField) const {
-    return global_ptr<F>(region_, this->*ptrField.offset_);
+  const global_ptr<F, RT> get(regional_ptr<F> VT::*ptrField) const {
+    return global_ptr<F, RT>(region_, this->*ptrField.offset_);
   }
 
-  T *get() {
+  VT *get() {
     if (offset_ == 0)
       return nullptr;
-    return reinterpret_cast<T *>(reinterpret_cast<intptr_t>(region_) + offset_);
+    return reinterpret_cast<VT *>(reinterpret_cast<intptr_t>(region_) + offset_);
   }
 
-  const T *get() const {
+  const VT *get() const {
     if (offset_ == 0)
       return nullptr;
-    return reinterpret_cast<const T *>(reinterpret_cast<intptr_t>(region_) + offset_);
+    return reinterpret_cast<const VT *>(reinterpret_cast<intptr_t>(region_) + offset_);
   }
 
-  T &operator*() { return *get(); }
-  T *operator->() { return get(); }
-  const T &operator*() const { return *get(); }
-  const T *operator->() const { return get(); }
+  VT &operator*() { return *get(); }
+  VT *operator->() { return get(); }
+  const VT &operator*() const { return *get(); }
+  const VT *operator->() const { return get(); }
 
   explicit operator bool() const noexcept { return offset_ != 0; }
 
-  bool operator==(const global_ptr<T> &other) const { return other.region_ == region_ && other.offset_ == offset_; }
+  bool operator==(const global_ptr<VT, RT> &other) const {
+    return other.region_ == region_ && other.offset_ == offset_;
+  }
 
-  bool operator!=(const global_ptr<T> &other) const { return other.region_ != region_ || other.offset_ != offset_; }
+  bool operator!=(const global_ptr<VT, RT> &other) const {
+    return other.region_ != region_ || other.offset_ != offset_;
+  }
 };
 
 } // namespace shilos
